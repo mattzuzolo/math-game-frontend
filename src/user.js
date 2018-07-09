cff7a67f462e9081f1369c3466f61c8e2592af0e
github username <github email address>
2018-07-09 16:09:57 -0400

    updating front

--- src/user.js
+++ src/user.js
@@ -1,11 +1,11 @@
-// let localUserId = 0;
-//
-// class User {
-//   constructor(obj){
-//     this.localUserId = ++localUserId;
-//     this.name = obj.name;
-//     this.apiId = obj.id;
-//   }
-//
-//
-// }
+let localUserId = 0;
+
+class User {
+  constructor(obj){
+    this.localUserId = ++localUserId;
+    this.name = obj.name;
+    this.apiId = obj.id;
+  }
+
+
+}
