2ac00f1d7eeafa6d4e631c545bc4a18c66c2d4c7
github username <github email address>
2018-07-10 13:06:03 -0400

    improved code readability

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-
 //local storage
 let store = {user: [], game: []}
 
@@ -21,219 +20,24 @@ fetch(userUrl)
 .then(response=>response.json())
 .then(data=>displayUser(data))
 
+//Scoreboard display
 function displayUser(data){
-
   data.forEach(function(individualPlayer){
     playerUl.innerHTML+=`<li>${individualPlayer.name}<ul id=${individualPlayer.id}></ul></li>`
     displayIndividualGame(individualPlayer.games)
   })
 }
-
 function displayIndividualGame(games) {
     games.forEach(function(individualGame){
       document.getElementById(`${individualGame.user_id}`).innerHTML+=`<li>Game${individualGame.id}:${individualGame.score}</li>`
     })
 }
 
-//Create new user in backend using POST
-// name is obtained from the HTML fields
-function addUserBackend(name){
-  let submissionBody = {
-    "name": name,
-  };
-
-  function addUser(userUrl,submissionBody) {
-     const postConfig = {
-       Accept: "application/json",
-       method: "POST",
-       headers: {
-         "Content-type": "application/json"
-       },
-       body: JSON.stringify(submissionBody)
-     };
-     return fetch(userUrl, postConfig)
-   }
-   addUser(userUrl,submissionBody);
-}
-
-
-function addGameBackend(userId, score){
-  console.log("Your score will be submitted to the API momentarily")
-  let submissionBody = {
-    "user_id": userId,
-    "score": score
-  }
-
-  function addGame(gameUrl,submissionBody) {
-     const postConfig = {
-       Accept: "application/json",
-       method: "POST",
-       headers: {
-         "Content-type": "application/json"
-       },
-       body: JSON.stringify(submissionBody)
-     };
-     return fetch(gameUrl, postConfig)
-   }
-   addGame(gameUrl,submissionBody)
-}
-
-document.getElementById("start-game").addEventListener("click", gameSetup)
-
-function countdown(timer){
-  let startingTime = 60; //60 seconds on clock to start
-  setInterval(function(){
-    startingTime--;
-    if (startingTime >= 0){
-      timer.innerText = `There are ${startingTime} seconds remaining!`
-    }
-  }, 1000)
-}
-
-function answerHandling(answerForm, question, inputField, strikes, correctCounter, correctCounterNum, activeScore){
-
-// debugger;
-  let currentQuestion = mathQuiz();
-  // let currentAnswer = answer;
-  let userAnswer;
-  let newStrike = "X"
-  // let newCorrect = "|"
-
-  question.innerText = currentQuestion;
-  strikes.innerText = "Current strikes: ";
-
-  answerForm.addEventListener("click", function(e){
-      e.preventDefault();
-      if (e.target.id === "submit-answer"){
-
-        let userAnswer = parseInt(e.target.parentElement.getElementsByTagName("INPUT")[0].value);
 
 
 
-        if (userAnswer == answer){
-          // alert("Correct!")
-          // debugger;
-          activeScore++ //= parseInt(++correctCounterNum)
-
-          document.getElementById('user-input').value = '';
-          correctCounter.innerText =`You have correctly answered: ${activeScore}`;
-          answerHandling(answerForm, question, strikes);
-        }
-        else {
-          strikes.innerText += newStrike;
-          // display next question if the answer is incorrect
-            currentQuestion = mathQuiz()
-            question.innerText = currentQuestion;
-          if (strikes.innerText == "Current strikes:XXX"){
-            //alert("GAME OVER!")
-            gameOver(activeScore);
-          }
-
-        }
-
-      }
-  })
-
-}
-
-function expireTime(){
-  // let startingTime = 60; //60 seconds on clock to start
-  // startingTime--;
-  window.setTimeout(function(){
-    gameOver();
-  }, 60000)
-}
-
-function gameOver(activeScore){
-    disableGameplay();
-    alert("GAME OVER!!!")
-    let continueToScoreboard = document.createElement("h1");
-    continueToScoreboard.innerText = "Click here to see the scoreboard"
-    continueToScoreboard.style.textAlign = "center";
-    postGameContainer.append(continueToScoreboard);
-    //access real userId when we have OO ready
-    let playerName = loginField.value
-    let userId = getUserId(playerName);
-    addGameBackend(userId, activeScore)
-}
-
-function getUserId(playerName){
-  if (playerName === "Matt"){
-    return 1;
-  }
-  else if (playerName === "Steven"){
-    return 2;
-  }
-  else{
-    return null;
-  }
-}
-
-function disableGameplay(){
-  document.getElementById("submit-answer").disabled = true;
-  timerContainer.remove(document.getElementById("timer-text"))
-
-  let gameOverText = document.createElement("h4");
-  gameOverText.innerText = "GAME OVER!"
-  timerContainer.append(gameOverText);
-
-}
-
-function gameSetup() {
-
-    //create timer
-    let timer = document.createElement("h4");
-    timer.style.textAlign = "center";
-    timer.id = "timer-text"
-    expireTime()
-    countdown(timer)
-    timerContainer.append(timer);
-
-    //create question
-    let question = document.createElement("h2");
-    question.style.textAlign = "center";
-    //question.innerText = currentQuestion
-    questionContainer.append(question);
-
-    let correctCounter = document.createElement("h1")
-    let correctCounterNum = 0;
-    correctCounter.style.textAlign = "center";
-    correctCounter.innerText = `You have correctly answered: `;
-    correctCounterContainer.append(correctCounter)
-
-    //strikes
-    let strikes = document.createElement("h1");
-    //strikes.innerText = "Current strikes: ";
-    strikes.style.textAlign = "center";
-    strikes.style.color = "red";
-    strikeContainer.append(strikes)
-
-
-    //create answer form
-    let answerForm = document.createElement("form")
-    let inputField = document.createElement("input")
-    let submitButton = document.createElement("button")
-
-    answerForm.style.textAlign = "center";
-    submitButton.innerText = "Submit answer"
-
-    answerForm.id = "answer-form"
-    inputField.id = "user-input"
-    submitButton.id = "submit-answer"
-
-    answerForm.append(inputField);
-    answerForm.append(submitButton);
-    answerContainer.append(answerForm);
-
-    let activeScore = 0;
-
-    answerHandling(answerForm, question, inputField, strikes, correctCounter, correctCounterNum, activeScore);
-
-}
-
-
-
-
+//Gameplay functionality
+//generates new question
 function mathQuiz() {
 
   //determintes question type (+, -, *, /)
@@ -270,5 +74,194 @@ function mathQuiz() {
   }
 
 }
+
+//causing game to end when parameters are met
+function gameOver(activeScore){
+    disableGameplay();
+    alert("GAME OVER!!!")
+    let continueToScoreboard = document.createElement("h1");
+    continueToScoreboard.innerText = "Click here to see the scoreboard"
+    continueToScoreboard.style.textAlign = "center";
+    postGameContainer.append(continueToScoreboard);
+    //access real userId when we have OO ready
+    let playerName = loginField.value
+    let userId = getUserId(playerName);
+    addGameBackend(userId, activeScore)
+}
+
+//disables DOM functionality so user cannot continue playing after game is over
+function disableGameplay(){
+  document.getElementById("submit-answer").disabled = true;
+  timerContainer.remove(document.getElementById("timer-text"))
+
+  let gameOverText = document.createElement("h4");
+  gameOverText.innerText = "GAME OVER!"
+  timerContainer.append(gameOverText);
+}
+
+//this function obtains user answers and resets question and answer so use can continue playing
+//this function operates recursively so that the user can continue playing after answering a question
+function answerHandling(answerForm, question, inputField, strikes, correctCounter, correctCounterNum, activeScore){
+  let currentQuestion = mathQuiz();
+  let userAnswer;
+  let newStrike = "X"
+  question.innerText = currentQuestion;
+  strikes.innerText = "Current strikes: ";
+
+  answerForm.addEventListener("click", function(e){
+      e.preventDefault();
+      if (e.target.id === "submit-answer"){
+        let userAnswer = parseInt(e.target.parentElement.getElementsByTagName("INPUT")[0].value);
+
+        //check user answer against actual answer
+        if (userAnswer == answer){
+          activeScore++
+          document.getElementById('user-input').value = ''; //resets input field to ready for next question
+          correctCounter.innerText =`You have correctly answered: ${activeScore}`; //updates the current score
+          answerHandling(answerForm, question, strikes); //
+        }
+        else {
+          strikes.innerText += newStrike;
+          // display next question if the answer is incorrect
+            currentQuestion = mathQuiz()
+            question.innerText = currentQuestion;
+
+            if (strikes.innerText == "Current strikes:XXX"){
+              gameOver(activeScore);
+            }
+        }
+      }
+  })
+
+}
+
+//This method setups the DOM for the game.
+//Also starts the timer
+function gameSetup() {
+
+    //create timer
+    let timer = document.createElement("h4");
+    timer.style.textAlign = "center";
+    timer.id = "timer-text"
+    expireTime()
+    countdown(timer)
+    timerContainer.append(timer);
+
+    //create question
+    let question = document.createElement("h2");
+    question.style.textAlign = "center";
+    //question.innerText = currentQuestion
+    questionContainer.append(question);
+
+    let correctCounter = document.createElement("h1")
+    let correctCounterNum = 0;
+    correctCounter.style.textAlign = "center";
+    correctCounter.innerText = `You have correctly answered: `;
+    correctCounterContainer.append(correctCounter)
+
+    //strikes
+    let strikes = document.createElement("h1");
+    //strikes.innerText = "Current strikes: ";
+    strikes.style.textAlign = "center";
+    strikes.style.color = "red";
+    strikeContainer.append(strikes)
+
+    //create answer form
+    let answerForm = document.createElement("form")
+    let inputField = document.createElement("input")
+    let submitButton = document.createElement("button")
+
+    answerForm.style.textAlign = "center";
+    submitButton.innerText = "Submit answer"
+
+    answerForm.id = "answer-form"
+    inputField.id = "user-input"
+    submitButton.id = "submit-answer"
+
+    answerForm.append(inputField);
+    answerForm.append(submitButton);
+    answerContainer.append(answerForm);
+
+    let activeScore = 0; //define here so score doesn't reset after
+
+    answerHandling(answerForm, question, inputField, strikes, correctCounter, correctCounterNum, activeScore);
+}
+
+
+
+//These functions manage GET and POST requests with FETCH API
+function addUserBackend(name){
+  let submissionBody = {
+    "name": name,
+  };
+
+  function addUser(userUrl,submissionBody) {
+     const postConfig = {
+       Accept: "application/json",
+       method: "POST",
+       headers: {
+         "Content-type": "application/json"
+       },
+       body: JSON.stringify(submissionBody)
+     };
+     return fetch(userUrl, postConfig)
+   }
+   addUser(userUrl,submissionBody);
+}
+function addGameBackend(userId, score){
+  console.log("Your score will be submitted to the API momentarily")
+  let submissionBody = {
+    "user_id": userId,
+    "score": score
+  }
+
+  function addGame(gameUrl,submissionBody) {
+     const postConfig = {
+       Accept: "application/json",
+       method: "POST",
+       headers: {
+         "Content-type": "application/json"
+       },
+       body: JSON.stringify(submissionBody)
+     };
+     return fetch(gameUrl, postConfig)
+   }
+   addGame(gameUrl,submissionBody)
+}
+
+//Start the game by clicking start
+document.getElementById("start-game").addEventListener("click", gameSetup)
+
+function countdown(timer){
+  let startingTime = 60; //60 seconds on clock to start
+  setInterval(function(){
+    startingTime--;
+    if (startingTime >= 0){
+      timer.innerText = `There are ${startingTime} seconds remaining!`
+    }
+  }, 1000)
+}
+
+function expireTime(){
+  // let startingTime = 60; //60 seconds on clock to start
+  // startingTime--;
+  window.setTimeout(function(){
+    gameOver();
+  }, 60000)
+}
+
+function getUserId(playerName){
+  if (playerName === "Matt"){
+    return 1;
+  }
+  else if (playerName === "Steven"){
+    return 2;
+  }
+  else{
+    return null;
+  }
+}
+
+
 let button=document.getElementById("button")
 button.addEventListener("click",gameSetup)
